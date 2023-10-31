6effe56fbe06630d644b539c806732f8923d4526
Olivier Jean <oliviano@gmail.com>
2023-10-31 13:09:57 +1300

    Create RemoteShutdown.mk

---
+++ RemoteShutdown.mk
@@ -0,0 +1,8 @@
+##Remote shutdown with Remote Registery Properties.
+
+-> enable services "remote registery"
+-> enable port through firewall. 
+-> run command.
+
+source:
+https://www.howtogeek.com/109655/how-to-remotely-shut-down-or-restart-windows-pcs/

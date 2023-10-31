124d00cab34d4df5a820e2ee70b63ecd58490b24
Olivier Jean <oliviano@gmail.com>
2023-10-31 15:49:17 +1300

    Updated to allow for Remote Registry Config

--- RemoteShutdown.mk
+++ RemoteShutdown.mk
@@ -6,3 +6,11 @@
 
 source:
 https://www.howtogeek.com/109655/how-to-remotely-shut-down-or-restart-windows-pcs/
+
+to enable / disable:
+LocalAccountTokenFilterPolicy accessing the C$ with a local account
+
+-> with ps:
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system -Name LocalAccountTokenFilterPolicy -Value 0
+-> with cmd:
+cmd /c reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
